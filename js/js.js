e5df42c0a17cc567a758c5e5b93ee8814db561c8
L4NK0R <63055083+L4NK0R@users.noreply.github.com>
2022-06-08 21:09:29 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,8 +1,10 @@
 'use strict'
 let buttons = document.querySelectorAll(".leftright");
 let slideshowpolotno = document.querySelector(".slidepolotno");
-let transcount = 0;
+let transcount = -100;
 let count = 0;
+slideshowpolotno.style.transform = "translate(-100%)";
+slideshowpolotno.style.transition = "0s";
 // let transfun =
 // transform: translate(transcount+"%");
 buttons.forEach(item => item.addEventListener("click", slide));
@@ -18,17 +20,32 @@ function slide(event){
         count++;
     }
     if(count<0){
-        transcount = -500;
-        console.log(count);
-        slideshowpolotno.style.transform = `translate(${transcount}%)`
+          //a1
         count = 5;
+        setTimeout(gotoA1, 1000);
     }else if(count>5){
-        transcount = 0;
-        slideshowpolotno.style.transform = `translate(${transcount}%)`
+         //a6   
         count = 0;
+        setTimeout(gotoA6, 1000);
     }
     
 }
+
+function gotoA1(){
+    slideshowpolotno.style.transition = "0s";
+    transcount = -600;
+    slideshowpolotno.style.transform = `translate(${transcount}%)`
+    setTimeout(function(){slideshowpolotno.style.transition = "1s"},100);
+}
+
+function gotoA6(){
+    slideshowpolotno.style.transition = "0s";
+    transcount = -100;
+    slideshowpolotno.style.transform = `translate(${transcount}%)`
+    count = 0;
+    setTimeout(function(){slideshowpolotno.style.transition = "1s"},100)
+}
+
 // Функция времени, вызывается из консоли, ибо пока не сделал функцию обрывания
 function inftimer(){
     setTimeout(infslide, 3000);
@@ -41,3 +58,5 @@ function infslide(){
     }
     inftimer();
 }
+
+setTimeout(function (){slideshowpolotno.style.transition = "1s";},0);
